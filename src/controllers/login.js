4527333156bb7195a7417088b4741f0feb04fc57
TechsavvYash <mittal.yash12@hotmail.com>
2022-07-17 21:18:43 +0530

    added prettier rc
    Signed-off-by: TechsavvYash <mittal.yash12@hotmail.com>

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,47 +1,52 @@
-const path = require('path')
-const generateJWT = require('../util/utils').generateJWT
-
+const path = require("path");
+const generateJWT = require("../util/utils").generateJWT;
 
 exports.getLogin = (req, res) => {
-    if(req.user == null) res.sendFile(path.join(__dirname, '../../public/pages/login.html'))
-    else res.redirect('/success')
-}
+  if (req.user == null)
+    res.sendFile(path.join(__dirname, "../../public/pages/login.html"));
+  else res.redirect("/success");
+};
 
 exports.postLogin = (req, res) => {
-    const { username, pwd} = req.body
-    const user = req.user ; 
-    if(req.user == null) {
-      res.send({status: false, message: "invalid credentials"}) ;
-      return ;
+  const { username, pwd } = req.body;
+  const user = req.user;
+  if (req.user == null) {
+    res.send({ status: false, message: "invalid credentials" });
+    return;
+  }
+
+  try {
+    const enteredPwd = pwd.split(",");
+    const userPwd = user.pwd.split(",");
+    //console.log("Entered Pwd: ", enteredPwd) ;
+    //console.log("UserPwd: ", userPwd)
+    if (enteredPwd.length !== userPwd.length) {
+      res.send({ status: false, message: "invalid credentials" });
+      return;
     }
-    
-    try {
-      const enteredPwd = pwd.split(',') ;
-      const userPwd = user.pwd.split(',') ;
-      //console.log("Entered Pwd: ", enteredPwd) ;
-      //console.log("UserPwd: ", userPwd)
-      if(enteredPwd.length !== userPwd.length) {
-        res.send({status: false, message: "invalid credentials"}) ;
-        return ; 
+    let isAMatch = true;
+    for (let i = 0; i < enteredPwd.length; i++) {
+      if (
+        !(
+          parseFloat(enteredPwd[i]) <= parseFloat(userPwd[i]) + 10 &&
+          parseFloat(enteredPwd[i]) >= parseFloat(userPwd[i]) - 10
+        )
+      ) {
+        console.log("False at: ", i + 1);
+        isAMatch = false;
+        break;
       }
-      let isAMatch = true ;
-      for(let i = 0 ; i < enteredPwd.length; i++) {
-        if(!(parseFloat(enteredPwd[i]) <= (parseFloat(userPwd[i]) + 10) && parseFloat(enteredPwd[i]) >= (parseFloat(userPwd[i]) - 10))) {
-          console.log("False at: ", i+1);
-          isAMatch = false ;
-          break ;
-        }
-      }
-      if(!isAMatch) {
-        res.send({status: false, message: "invalid credentials"})
-      } else {
-        req.session.token = generateJWT(user.username) ;
-        res.send({status: true, message: "login succesful"})
-        //res.redirect('/success')
-        //res.render("success", {text: "Login Successful"})
-      }
-    } catch(err) {
-      console.log(err);
-      res.send({status: false, message: err})
     }
-}
+    if (!isAMatch) {
+      res.send({ status: false, message: "invalid credentials" });
+    } else {
+      req.session.token = generateJWT(user.username);
+      res.send({ status: true, message: "login succesful" });
+      //res.redirect('/success')
+      //res.render("success", {text: "Login Successful"})
+    }
+  } catch (err) {
+    console.log(err);
+    res.send({ status: false, message: err });
+  }
+};
